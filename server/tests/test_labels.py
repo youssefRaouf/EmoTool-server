9bafb24b0346721dc53abea871a5de935f67c0f5
Hagrass <bodyhagrass@gmail.com>
2022-05-19 19:43:43 +0200

    adding Label Tests

--- server/tests/test_labels.py
+++ server/tests/test_labels.py
@@ -1,5 +1,40 @@
 from django.test import SimpleTestCase
-
+from server.routes import classify_tweet, classify_tweets
 class TestLabels(SimpleTestCase):
-    def test1(self):
-        assert 1==2
+    def test_ClassifySingleTweet(self):
+
+        tweet_dict = [{'text':'I love you',
+                       'label':'joy'},
+
+                      {'text':'I am very angry',
+                       'label':'anger'
+                      }]
+
+        # Assert
+        for tweet in tweet_dict:
+            self.assertEquals(classify_tweet(tweet['text']),tweet['label'])
+
+
+
+
+    def test_ClassiftMultipleTweets(self):
+        tweet_dict = {
+        'texts':[
+            'I love you',
+            "I am very angry"
+        ],
+        'labels':[
+            'joy',
+            'anger'
+                      ]}
+
+        self.assertEquals(classify_tweets(tweet_dict['texts']),tweet_dict['labels'])
+
+
+
+
+
+
+
+
+
---
+++ server/tests/test_runtime.py
@@ -0,0 +1,5 @@
+from django.test import SimpleTestCase
+
+class TestLabels(SimpleTestCase):
+    def test1(self):
+        assert 2==2
