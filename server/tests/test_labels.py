81ac4e562a7f82a039d65dc00a8f70b4bfadecda
Hagrass <bodyhagrass@gmail.com>
2022-05-19 20:16:47 +0200

    adding Label And Runtime Tests

--- server/tests/test_labels.py
+++ server/tests/test_labels.py
@@ -1,34 +1,39 @@
 from django.test import SimpleTestCase
 from server.routes import classify_tweet, classify_tweets
+
+ClassifySingle_tweet_dict = [{'text': 'I love you',
+                              'label': 'joy'},
+
+                             {'text': 'I am very angry',
+                              'label': 'anger'
+                              }]
+
+ClassifyMultiple_tweet_dict = {
+    'texts': [
+        'I love you',
+        "I am very angry"
+    ],
+    'labels': [
+        'joy',
+        'anger'
+    ]}
 class TestLabels(SimpleTestCase):
+
+
     def test_ClassifySingleTweet(self):
 
-        tweet_dict = [{'text':'I love you',
-                       'label':'joy'},
 
-                      {'text':'I am very angry',
-                       'label':'anger'
-                      }]
 
         # Assert
-        for tweet in tweet_dict:
+        for tweet in ClassifySingle_tweet_dict:
             self.assertEquals(classify_tweet(tweet['text']),tweet['label'])
 
 
 
 
     def test_ClassiftMultipleTweets(self):
-        tweet_dict = {
-        'texts':[
-            'I love you',
-            "I am very angry"
-        ],
-        'labels':[
-            'joy',
-            'anger'
-                      ]}
 
-        self.assertEquals(classify_tweets(tweet_dict['texts']),tweet_dict['labels'])
+        self.assertEquals(classify_tweets(ClassifyMultiple_tweet_dict['texts']),ClassifyMultiple_tweet_dict['labels'])
 
 
 
